f6e17504f13eb07480831426d94ddd116af695d2
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 16:56:34 +0700

    update

--- flake.nix
+++ flake.nix
@@ -14,6 +14,9 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
+        config = {
+                allowUnfree = true;
+            };
       };
     in
     {
