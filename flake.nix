f537fd34c701df8a2b56fefc33a4695db9349275
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 17:05:51 +0700

    update

--- flake.nix
+++ flake.nix
@@ -4,17 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, utils}:
+  outputs = { self, nixpkgs, home-manager }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        config = {
-                allowUnfree = true;
-            };
       };
     in
     {
@@ -27,7 +23,9 @@
       #     buildInputs =
       #       [
       #         pkgs.bat
-      #         pkgs.git pkgs.gdu ];
+      #         pkgs.git
+      #         pkgs.gdu
+      #       ];
       #   };
       # };
 
@@ -38,9 +36,8 @@
         };
       };
 
-      devShells.x86_64-linux = import ./devShells.nix {
-          pkgs = utils.lib.legacyPackages.${system};
-      };
+      devShells.x86_64-linux = import ./devShells.nix { 
+        inherit pkgs;   
+          };
     };
 }
-
