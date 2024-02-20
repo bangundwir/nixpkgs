860682c03cdbf62fbeec743b30cbf773f4f7782c
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 18:18:03 +0700

    direnv

--- home/home.nix
+++ home/home.nix
@@ -1,30 +1,38 @@
-{pkgs, ...}: {
-    home.username = "hades";
-    home.homeDirectory = "/home/hades";
-    home.packages = with pkgs; [
-        cowsay
-        bat
-        ranger
-        vim
-        btop
-        gdu
+{ pkgs, ... }: {
+  home.username = "hades";
+  home.homeDirectory = "/home/hades";
+  home.packages = with pkgs; [
+    cowsay
+    bat
+    ranger
+    vim
+    btop
+    gdu
 
-        zsh
-        neofetch
-        unzip
-        fzf
-        fzf-zsh
+    zsh
+    neofetch
+    unzip
+    fzf
+    fzf-zsh
 
-        #font
-        fira-code
-        direnv
-        nix-direnv
+    #font
+    fira-code
+    # noto-fonts
+    # noto-fonts-cjk
+    # noto-fonts-emoji
+    # liberation_ttf
+    fira-code
+    fira-code-symbols
+    nerdfonts
+    ##
+    direnv
+    nix-direnv
 
-    ];
-    home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
-    programs.home-manager.enable = true;
-    # font.fontconfig.enable = true;
-    programs = {
+  ];
+  home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
+  programs.home-manager.enable = true;
+  # font.fontconfig.enable = true;
+  programs = {
     direnv = {
       enable = true;
       # enableBashIntegration = true; # see note on other shells below
