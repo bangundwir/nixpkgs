16b3e16f2e3b08816a9d8b8cdbb661a331f20fd7
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 17:28:57 +0700

    font

--- home/home.nix
+++ home/home.nix
@@ -12,5 +12,5 @@
     ];
     home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
-    font.fontconfig.enable = true;
+    # font.fontconfig.enable = true;
 }
