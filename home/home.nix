b2187ffae710f0af38ec5b9932689a5ba1f53366
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 17:27:16 +0700

    update

--- home/home.nix
+++ home/home.nix
@@ -4,8 +4,13 @@
     home.packages = with pkgs; [
         cowsay
         bat
+        ranger
+
+        #font
+        fire-code
 
     ];
     home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
+    font.fontconfig.enable = true;
 }
