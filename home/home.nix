6f3ea527c6fc2a58b8ef99b9788f460231f09c73
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 17:37:28 +0700

    font

--- home/home.nix
+++ home/home.nix
@@ -17,9 +17,20 @@
 
         #font
         fira-code
+        direnv
+        nix-direnv
 
     ];
     home.stateVersion = "22.11"; # To figure this out you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
     # font.fontconfig.enable = true;
+    programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
+  };
 }
