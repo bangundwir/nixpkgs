0158d71aaae27fedbfc6744e1f05ab83a6e1e312
bangundwir <hendrabangundwir@gmail.com>
2024-02-20 17:42:59 +0700

    direnv

--- home/home.nix
+++ home/home.nix
@@ -27,7 +27,7 @@
     programs = {
     direnv = {
       enable = true;
-      enableBashIntegration = true; # see note on other shells below
+      # enableBashIntegration = true; # see note on other shells below
       nix-direnv.enable = true;
     };
 
